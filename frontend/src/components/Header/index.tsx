8efc8add14cc62d7f2eeeed3ff2040a84eb5b37e
CarlosReategui <carlos.reategui@utec.edu.pe>
2023-03-23 13:38:03 -0500

    [FRONTEND] fix header error

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,6 +5,7 @@ export const AppHeader = () => {
   const { logout } = useAuth();
   const theme = useMantineTheme();
   return (
+    <Header height={75} mb="xl">
       <Flex align="center" direction="column" mt="md">
         {/* <Text color="dimmed">
           This is supposed to be a header, but it's not implemented yet.
